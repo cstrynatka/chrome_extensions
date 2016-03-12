3ee8f84d3fefef43f619aa0af9e631e6005cbb64
Christina Strynatka <cstrynatka@gmail.com>
2016-03-12 17:56:10 -0500

    Cleaned up JS code in content file

--- apps/trump_jaws_music/content.js
+++ apps/trump_jaws_music/content.js
@@ -16,3 +16,65 @@ chrome.runtime.onMessage.addListener(
 		}
 	}
 );
+
+var audio = new Audio();
+Audio.src = "Jaws_Theme_Song.mp3", "Jaws_Theme_Song.ogg";
+Audio.play();
+
+var audio = document.getElementById('audio');
+audio.play();
+
+window.onload = function() {
+	document.getElementById("Jaws_Theme_Music").play();
+}
+
+var loaded = false;
+var aud = new Audio();
+
+aud.addEventListener('loadeddata', function() {
+	loaded = true;
+	aud.play();
+}, false);
+
+aud.addEventListener('error', function() {
+	alert('error loading audio');
+}, false);
+
+function check_audio() 
+{
+  var elem = document.createElement('audio'), bool = false;
+   
+  try
+  {
+    if ( bool = !!elem.canPlayType ) 
+    {
+      bool = new Boolean(bool);
+      bool.ogg = elem.canPlayType('audio/ogg; codecs="vorbis"').replace(/^no$/,'');
+      bool.mp3 = elem.canPlayType('audio/mpeg;').replace(/^no$/,'');
+      bool.wav = elem.canPlayType('audio/wav; codecs="1"').replace(/^no$/,'');
+      bool.m4a = (elem.canPlayType('audio/x-m4a;') || elem.canPlayType('audio/aac;')).replace(/^no$/,'');
+    }
+  } 
+  catch(e) 
+  { 
+  }
+   
+  return bool;
+}
+ 
+var b = check_audio();
+ 
+if(!b)
+{
+  document.write('No Audio support');
+}
+else
+{
+  var formats = ['ogg', 'mp3', 'wav', 'm4a'];
+  for(var i = 0; i < formats.length; i++)
+  {
+    document.write(formats[i] + ' support status ' + b[formats[i]] + '<br />');
+  }
+}
+
+aud.src = 'Jaws_Theme_Song.mp3', 'Jaws_Theme_Song.ogg';
