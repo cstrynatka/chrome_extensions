7acb1b8e9b38f8deaf771f295df0c85c7e719c59
Christina Strynatka <cstrynatka@gmail.com>
2016-03-11 21:29:44 -0500

    Added icons

---
+++ apps/trump_jaws_music/content.js
@@ -0,0 +1,18 @@
+// Content for the extension that'll do cool stuff on the page
+// alert("Here is the theme song to Donald Trump!")
+
+chrome.runtime.onMessage.addListener(
+	function(request, sender, sendResponse) {
+		if( request.message === "clicked_browser_action" ) {
+			var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+			console.log(firstHref);
+
+			// Adding in more info to the action so that clicking on the browser action will trigger
+			// the URL to be sent back to the background.js file and a new tab will open
+			chrome.runtime.sendMessage( {
+				"message": "open_new_tab", "url": firstHref
+			});
+		}
+	}
+);
