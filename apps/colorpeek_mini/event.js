6eef2d763ae734ac735057b0bac9f828ec51d579
Christina Strynatka <cstrynatka@gmail.com>
2016-03-11 21:33:11 -0500

    Cleaned up manifest JSON file

--- apps/colorpeek_mini/event.js
+++ apps/colorpeek_mini/event.js
@@ -1,5 +1,3 @@
-// Execute the inject.js in a tab and call a method,
-// passing the result to a callback function.
 function injectedMethod (tab, method, callback) {
   chrome.tabs.executeScript(tab.id, { file: 'inject.js' }, function(){
     chrome.tabs.sendMessage(tab.id, { method: method }, callback);
@@ -7,14 +5,16 @@ function injectedMethod (tab, method, callback) {
 }
 
 function getBgColors (tab) {
-  // When we get a result back from the getBgColors
-  // method, alert the data
   injectedMethod(tab, 'getBgColors', function (response) {
-    alert('Elements in tab: ' + response.data);
+    var colors = response.data;
+    if (colors && colors.length) {
+      var url = 'http://colorpeek.com/#' + colors.join(',');
+      chrome.tabs.create({ url: url });
+    } else {
+      alert('No background colors were found! :(');
+    }
     return true;
-  });
+  })
 }
 
-// When the browser action is clicked, call the
-// getBgColors function.
 chrome.browserAction.onClicked.addListener(getBgColors);
--- apps/colorpeek_mini/inject.js
+++ apps/colorpeek_mini/inject.js
@@ -1,29 +1,39 @@
-// This helps avoid conflicts in case we inject 
-// this script on the same page multiple times
-// without reloading.
 var injected = injected || (function(){
 
-  // An object that will contain the "methods"
-  // we can use from our event script.
   var methods = {};
 
-  // This method will eventually return
-  // background colors from the current page.
   methods.getBgColors = function(){
+    var colors = {};
     var nodes = document.querySelectorAll('*');
-    return nodes.length;
-  };
+    // return nodes.length;
+    var node, nodeArea, bgColor, i;
 
-  // This tells the script to listen for
-  // messages from our extension.
+    for (i = 0; i < nodes.length; i++) {
+      nodes = nodes[i];
+
+      nodeArea = node.clientWidth * node.clientHeight;
+
+      bgColor = bgColor.replace(/ /g, '');
+
+      if (
+        bgColor != 'rgb(255,255,255)' &&
+        !(bgColor.indexOf('rgba') === 0 && bgColor.substr(-3) === ',0)')
+        ) {
+        colors[bgColor] = (colors[bgColor] >> 0) + nodeArea;
+      }
+    }
+
+    return Object.getOwnPropertyNames(colors).sort(function (a,b) {
+      return colors[b] - colors[a];
+    });
+  }
+
   chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     var data = {};
-    // If the method the extension has requested
-    // exists, call it and assign its response
-    // to data.
+
     if (methods.hasOwnProperty(request.method))
       data = methods[request.method]();
-    // Send the response back to our extension.
+
     sendResponse({ data: data });
     return true;
   });
