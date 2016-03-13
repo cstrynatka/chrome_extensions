40185c700f4df4a0ab73a850af2032a14f1d308d
Christina Strynatka <cstrynatka@gmail.com>
2016-03-12 22:12:36 -0500

    Created manifest file

--- apps/trump_jaws_music/background.js
+++ apps/trump_jaws_music/background.js
@@ -1,28 +1,14 @@
-// This is what will happen when the user clicks on the designated browser action
-chrome.browserAction.onClicked.addListener(function(tab) {
-
-	// Send a message to the tab the user is currently on
-	chrome.tabs.query( {
-		active: true, currentWindow: true
-	},
-		function(tabs) {
-			var activeTab = tabs[0];
-			chrome.tabs.sendMessage(activeTab.id, {
-				"message": "clicked_browser_action"
-			});
-		});
-
-	// Next we need to receive the message sent by the new info in the content.js file so that we can 
-	// receive the URL and open a new tab
-	chrome.runtime.onMessage.addListener(
-		function(request, sender, sendResponse) {
-			if( request.message === "open_new_tab" ) {
-				chrome.tabs.create( {
-					"url": request.url
-				});
-			}
-		})
-});
+var timeout;
+textarea.addEventListener("keydown", function(e) {
+	clearTimeout(timeout);
+	self = e.target;
+	timeout = setTimeout(function(function) {
+		console.log(self.value);
+		if (self.value.indexOf("Donald Trump") > -1) {
+			alert("Here's the theme music to Donald Trump");
+		}
+	}, 500)
+})
 
 var audioElement = document.createElement('audio');
 audioElement.setAttribute("preload", "auto");
